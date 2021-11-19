1ea6353413f25c4dfa9097ee90d536656eb92e1d
mburuanthony <mburuantony@outlook.com>
2021-11-19 16:03:36 +0300

    Added Email SignIn and SignUp

---
+++ src/Firebase/emailAuth.js
@@ -0,0 +1,27 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./config";
+
+export const SignupWithEmailAndPAssword = ({ email, password }) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((res) => {
+      const user = res.user;
+      localStorage.setItem("user", user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const SignInWithEmailAndPassword = ({ email, password }) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((res) => {
+      const user = res.user;
+      localStorage.setItem("user", user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
